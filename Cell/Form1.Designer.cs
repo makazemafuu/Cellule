b288afc145a228932b9533957a04aee1870cd6cf
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-10-25 10:22:41 +0200

    Button, Affichage & code pour cell_Paint

--- Cell/Form1.Designer.cs
+++ Cell/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnl_cell = new System.Windows.Forms.Panel();
+            this.btn_simulation = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnl_cell
+            // 
+            this.pnl_cell.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pnl_cell.Location = new System.Drawing.Point(1106, 184);
+            this.pnl_cell.Name = "pnl_cell";
+            this.pnl_cell.Size = new System.Drawing.Size(907, 839);
+            this.pnl_cell.TabIndex = 0;
+            this.pnl_cell.Paint += new System.Windows.Forms.PaintEventHandler(this.pnl_cell_Paint);
+            // 
+            // btn_simulation
+            // 
+            this.btn_simulation.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_simulation.Location = new System.Drawing.Point(1405, 1050);
+            this.btn_simulation.Name = "btn_simulation";
+            this.btn_simulation.Size = new System.Drawing.Size(338, 87);
+            this.btn_simulation.TabIndex = 1;
+            this.btn_simulation.Text = "Simulation";
+            this.btn_simulation.UseVisualStyleBackColor = false;
+            this.btn_simulation.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(3140, 1268);
+            this.Controls.Add(this.btn_simulation);
+            this.Controls.Add(this.pnl_cell);
+            this.Name = "Form1";
+            this.Text = "Cellule Mutante";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel pnl_cell;
+        private Button btn_simulation;
     }
 }
--- Cell/Form1.cs
+++ Cell/Form1.cs
@@ -6,5 +6,29 @@ namespace Cell
         {
             InitializeComponent();
         }
+
+        private void btn_simulation_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pnl_cell_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.pnl_cell.CreateGraphics();
+            SolidBrush myBrush = new SolidBrush(Color.Black);
+            g.FillEllipse(myBrush, 120, 130, 20, 20);
+            myBrush.Dispose();
+            g.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
